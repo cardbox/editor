e524f51c52b50e286662fd369816a9d712834f42
risenforces <risenforces@gmail.com>
2021-06-01 21:12:44 +0500

    feature: getAbove now supports leaves

--- src/editor/common/queries.ts
+++ src/editor/common/queries.ts
@@ -1,7 +1,15 @@
-import { Editor } from 'slate'
+import { Editor, Range, Text } from 'slate'
+
+function hasSelection(editor: Editor): boolean {
+  if (!editor.selection) {
+    return false
+  }
+
+  return Range.isExpanded(editor.selection)
+}
 
 interface GetAboveOptions {
-  type: 'block'
+  type: 'block' | 'leaf'
   mode?: 'highest' | 'lowest'
 }
 
@@ -11,11 +19,13 @@ function getAbove(editor: Editor, options: GetAboveOptions) {
   return Editor.above(editor, {
     ...rest,
     match: (node) => {
+      if (type === 'leaf') return Text.isText(node)
       return Editor.isBlock(editor, node)
     },
   })
 }
 
 export const Queries = {
+  hasSelection,
   getAbove,
 }
