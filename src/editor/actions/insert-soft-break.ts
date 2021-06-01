3b370ad340b5bb5f7c4ae11636d65d7b1f6c9e43
risenforces <risenforces@gmail.com>
2021-06-01 14:04:05 +0500

    style: remove comment

--- src/editor/actions/insert-soft-break.ts
+++ src/editor/actions/insert-soft-break.ts
@@ -2,12 +2,6 @@ import { Editor } from 'slate'
 import { ActionCallback } from '../lib/action-controller/types'
 
 export const handler: ActionCallback<Editor> = (editor, event) => {
-  /*
-   * TODO: Implement place-break action
-   * It always bound to 'enter' key
-   * It used to start a new line with the default block
-   */
-
   const { selection } = editor
   if (!selection) return
 
