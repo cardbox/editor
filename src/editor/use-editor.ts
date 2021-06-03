c90d12fd75710451b0ac363ea4a4b05aac68ef7b
risenforces <risenforces@gmail.com>
2021-06-03 15:44:05 +0500

    feature: disable format as it have some problems

--- src/editor/use-editor.ts
+++ src/editor/use-editor.ts
@@ -2,36 +2,38 @@ import { useMemo } from 'react'
 import { createEditor } from 'slate'
 import { withHistory } from 'slate-history'
 import { withReact } from 'slate-react'
-import { Queries } from './common/queries'
 import { extensions } from './extensions'
 import { extend, Extension } from './lib/extensions/extend'
 
 const defaultExtensions: Extension[] = [
   withReact,
   withHistory,
-  extensions.format([
-    {
-      markupType: 'between',
-      markup: ['`', '`'],
-      transformType: 'leaf',
-      transform: ({ leaf, text }) => ({ ...leaf, text, inlineCode: true }),
-      skip: ({ leaf }) => Queries.leafHasModifications(leaf, ['inlineCode']),
-    },
-    {
-      markupType: 'between',
-      markup: ['**', '**'],
-      transformType: 'leaf',
-      transform: ({ leaf, text }) => ({ ...leaf, text, bold: true }),
-      skip: ({ leaf }) => Queries.leafHasModifications(leaf, ['bold']),
-    },
-    {
-      markupType: 'between',
-      markup: ['__', '__'],
-      transformType: 'leaf',
-      transform: ({ leaf, text }) => ({ ...leaf, text, italic: true }),
-      skip: ({ leaf }) => Queries.leafHasModifications(leaf, ['italic']),
-    },
-  ]),
+  // extensions.format([
+  //   {
+  //     trigger: '`',
+  //     markupType: 'between',
+  //     markup: ['`', '`'],
+  //     transformType: 'leaf',
+  //     transform: ({ leaf, text }) => ({ ...leaf, text, inlineCode: true }),
+  //     skip: ({ leaf }) => Queries.leafHasModifications(leaf, ['inlineCode']),
+  //   },
+  //   {
+  //     trigger: '*',
+  //     markupType: 'between',
+  //     markup: ['**', '**'],
+  //     transformType: 'leaf',
+  //     transform: ({ leaf, text }) => ({ ...leaf, text, bold: true }),
+  //     skip: ({ leaf }) => Queries.leafHasModifications(leaf, ['bold']),
+  //   },
+  //   {
+  //     trigger: '_',
+  //     markupType: 'between',
+  //     markup: ['__', '__'],
+  //     transformType: 'leaf',
+  //     transform: ({ leaf, text }) => ({ ...leaf, text, italic: true }),
+  //     skip: ({ leaf }) => Queries.leafHasModifications(leaf, ['italic']),
+  //   },
+  // ]),
 ]
 
 export function useEditor(customExtensions: Extension[]) {
